e96a3920a1f4aaac22c5d1cd89c8cbc19be8ab58
Getulio dos Santos <tulio.santos.araujo@hotmail.com>
2023-03-03 08:47:19 -0300

    update services to work with crud in api

--- farm_front/src/app/services/farm.service.ts
+++ farm_front/src/app/services/farm.service.ts
@@ -1,19 +1,34 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { environment } from 'src/environments/environment';
 import { Farm } from './../models/Farm'
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class FarmService {
-  constructor() {}
 
-  create(farm: Farm) {}
+  private readonly apiFarmUrl = environment.api_server + '/farms';
+  private readonly options = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
 
-  read(id: number): Farm {
-    return {} as any
+  constructor(private http: HttpClient) {}
+
+  create(farm: Farm) {
+    return this.http.post(`${this.apiFarmUrl}`, JSON.stringify(farm), this.options); 
   }
 
-  list(): Farm[] {
-    return []
+  read(id: number): Observable<Farm> {
+    return this.http.get<Farm>(`${this.apiFarmUrl}/${id}`); 
+  }
+
+  update(farm: Farm) {
+    return this.http.put(`${this.apiFarmUrl}`, JSON.stringify(farm), this.options); 
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${this.apiFarmUrl}/${id}`); 
+  }
+
+  list(): Observable<Farm[]> {
+    return this.http.get<Farm[]>(this.apiFarmUrl); 
   }
 }
--- farm_front/src/environments/environment.ts
+++ farm_front/src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  /** Endereço da API */
+  api_server: "http://localhost:3000",
+
 };
 
 /*
